f779aa3e7b3e54dff28fba3040d130a38d660089
anish-lkakapragada <anish.lakkapragada@gmail.com>
2023-03-22 19:15:46 -0700

    demo done.

--- main copy.py
+++ main copy.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from transformers import pipeline
 sentiment_pipeline = pipeline("sentiment-analysis")
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/predict/{text}")
